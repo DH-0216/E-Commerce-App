60d3170f90035172868462c9de3a4fb891d5f0c1
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-06-28 13:05:02 +0530

    update

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
@@ -6,6 +7,25 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          children: [
+            //logo
+            Image.asset(
+              'lib/images/png-clipart-nike-brand-adidas-asics-shoe-nike-angle-logo.png',
+              height: 240,
+              ),
+            
+        
+        
+            //title
+        
+            //sub title
+        
+            //start now button
+          ],
+        ),
+      ),
     );
   }
 }
