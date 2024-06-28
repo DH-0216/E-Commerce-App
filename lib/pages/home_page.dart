413ecf7a962ca7000b2aaa5ad730c38ad02a0f0e
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-06-28 18:42:57 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -54,7 +54,74 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.spaceBetween ,
+          children: [
+            Column(
+              children: [
+                 // logo
+                DrawerHeader(
+                  child: Image.asset(
+                    'lib/images/png-clipart-nike-brand-adidas-asics-shoe-nike-angle-logo.png',
+                    color: Colors.white,
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(
+                    color: Colors.grey[800],
+                  ),
+                ),
+
+                // other pages
+                const Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+
+                const Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'About',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+           
+            const Padding(
+              padding: const EdgeInsets.only(left:25.0,bottom: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout,color: Colors.white,),
+                title: Text('Logout',style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+
+
+
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
