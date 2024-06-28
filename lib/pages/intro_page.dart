cc13f41842f2cdaabb71f672f663c9e825deb041
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-06-28 13:58:56 +0530

    update

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
@@ -8,22 +10,68 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
-        child: Column(
-          children: [
-            //logo
-            Image.asset(
-              'lib/images/png-clipart-nike-brand-adidas-asics-shoe-nike-angle-logo.png',
-              height: 240,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/png-clipart-nike-brand-adidas-asics-shoe-nike-angle-logo.png',
+                  height: 240,
+                ),
               ),
-            
-        
-        
-            //title
-        
-            //sub title
-        
-            //start now button
-          ],
+
+              const SizedBox(height: 48),
+
+              //title
+              const Text(
+                'Just Do It',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 24),
+
+              //sub title
+              const Text(
+                'Brand new sneakers and custom kicks made with premium quality ',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 48),
+              //start now button
+
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context, 
+                  MaterialPageRoute(
+                    builder:(context) => HomePage(),
+                ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
