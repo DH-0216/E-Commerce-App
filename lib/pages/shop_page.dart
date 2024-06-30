42d99bf79d1ee2bcc88d58fe0025cb832bc77609
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-06-30 11:34:21 +0530

    Update shop_page.dart

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,8 +10,42 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('shop'),
+    return Column(
+      children: [
+        // search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+
+        //messaage
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+            'everyone flies.. some fly longer than others',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        )
+
+        //hot picks
+      ],
     );
   }
 }
