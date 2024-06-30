8ad54b910aabb31a50bbb5447dab2c01c4097521
Dulaj Hashmika <dulajhashmika075@gmail.com>
2024-06-30 11:34:11 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,9 +44,12 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         leading: Builder(
           builder: (context) => IconButton(
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.black,
+            icon: const Padding(
+              padding: EdgeInsets.only(left: 12.0),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
             ),
             onPressed: () {
               Scaffold.of(context).openDrawer();
@@ -57,11 +60,11 @@ class _HomePageState extends State<HomePage> {
       drawer: Drawer(
         backgroundColor: Colors.grey[900],
         child: Column(
-          mainAxisAlignment:MainAxisAlignment.spaceBetween ,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Column(
               children: [
-                 // logo
+                // logo
                 DrawerHeader(
                   child: Image.asset(
                     'lib/images/png-clipart-nike-brand-adidas-asics-shoe-nike-angle-logo.png',
@@ -70,7 +73,7 @@ class _HomePageState extends State<HomePage> {
                 ),
 
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Divider(
                     color: Colors.grey[800],
                   ),
@@ -78,7 +81,7 @@ class _HomePageState extends State<HomePage> {
 
                 // other pages
                 const Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
+                  padding: EdgeInsets.only(left: 25.0),
                   child: ListTile(
                     leading: Icon(
                       Icons.home,
@@ -92,7 +95,7 @@ class _HomePageState extends State<HomePage> {
                 ),
 
                 const Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
+                  padding: EdgeInsets.only(left: 25.0),
                   child: ListTile(
                     leading: Icon(
                       Icons.info,
@@ -104,21 +107,21 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-
               ],
             ),
-           
             const Padding(
-              padding: const EdgeInsets.only(left:25.0,bottom: 25),
+              padding: const EdgeInsets.only(left: 25.0, bottom: 25),
               child: ListTile(
-                leading: Icon(Icons.logout,color: Colors.white,),
-                title: Text('Logout',style: TextStyle(color: Colors.white),
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ),
-
-
-
           ],
         ),
       ),
